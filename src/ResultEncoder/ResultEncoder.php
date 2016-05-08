3f5a73a7a9e1158f4652989f78aaf86636ca5bf7
Ilija Studen <ilija.studen@activecollab.com>
2016-05-09 00:47:49 +0200

    Add Bootstrap level result encoder

---
+++ src/ResultEncoder/ResultEncoder.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Bootstrap project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+namespace ActiveCollab\Bootstrap\ResultEncoder;
+
+use ActiveCollab\Controller\ResultEncoder\ResultEncoder as BaseResultEncoder;
+
+/**
+ * @package ActiveCollab\Bootstrap\ResultEncoder
+ */
+class ResultEncoder extends BaseResultEncoder
+{
+}
