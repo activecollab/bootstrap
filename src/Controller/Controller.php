55c7e81f7f22304cf751ead4717055a8e3153c9c
Ilija Studen <ilija.studen@activecollab.com>
2016-05-14 22:27:32 +0200

    Add controller trait with getAuthenticatedUser method

---
+++ src/Controller/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Bootstrap project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+namespace ActiveCollab\Bootstrap\Controller;
+
+use ActiveCollab\Controller\Controller as BaseController;
+
+/**
+ * @package ActiveCollab\Bootstrap\Controller
+ */
+abstract class Controller extends BaseController
+{
+    use ControllerTrait;
+}
