f6b162daa635d021a1774e22ad502bdf0a339847
Ilija Studen <ilija.studen@activecollab.com>
2016-12-05 17:10:20 +0100

    Add getAuthenticatedWith() method to controllers

--- src/Controller/ControllerTrait.php
+++ src/Controller/ControllerTrait.php
@@ -8,7 +8,8 @@
 
 namespace ActiveCollab\Bootstrap\Controller;
 
-use ActiveCollab\User\UserInterface;
+use ActiveCollab\Authentication\AuthenticatedUser\AuthenticatedUserInterface;
+use ActiveCollab\Authentication\AuthenticationResult\AuthenticationResultInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 /**
@@ -19,10 +20,10 @@ trait ControllerTrait
     /**
      * Return authenticated user.
      *
-     * @param  ServerRequestInterface $request
-     * @return UserInterface|null
+     * @param  ServerRequestInterface          $request
+     * @return AuthenticatedUserInterface|null
      */
-    protected function getAuthenticatedUser(ServerRequestInterface $request)
+    protected function getAuthenticatedUser(ServerRequestInterface $request): ? AuthenticatedUserInterface
     {
         return $request->getAttribute($this->getAuthenticatedUserAttributeName());
     }
@@ -32,8 +33,29 @@ trait ControllerTrait
      *
      * @return string
      */
-    protected function getAuthenticatedUserAttributeName(): string
+    protected function getAuthenticatedUserAttributeName() : string
     {
         return 'authenticated_user';
+    }
+
+    /**
+     * Return authentication method instance (token or session).
+     *
+     * @param  ServerRequestInterface             $request
+     * @return AuthenticationResultInterface|null
+     */
+    protected function getAuthenticatedWith(ServerRequestInterface $request): ? AuthenticationResultInterface
+    {
+        return $request->getAttribute($this->getAuthenticatedWithAttributeName());
+    }
+
+    /**
+     * Return name of the request attribute where authentication method is stored.
+     *
+     * @return string
+     */
+    protected function getAuthenticatedWithAttributeName() : string
+    {
+        return 'authenticated_with';
     }
 }
