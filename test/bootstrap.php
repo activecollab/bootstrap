a226ddfacf1f395f714db8e00a3d5e7f7b084a96
Ilija Studen <ilija.studen@activecollab.com>
2016-12-08 22:42:28 +0100

    Fix test environment

---
+++ test/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Bootstrap project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+defined('BASE_PATH') || define('BASE_PATH', realpath(__DIR__));
+
+date_default_timezone_set('GMT');
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+require __DIR__ . '/src/Base/TestCase.php';
