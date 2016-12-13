7251b78792cff11a62258f78cd89ad1a9dadfabb
Ilija Studen <ilija.studen@activecollab.com>
2016-12-13 09:56:23 +0100

    Test access to authentication request attributes

---
+++ test/src/Fixtures/TestController.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Bootstrap project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Bootstrap\Test\Fixtures;
+
+use ActiveCollab\Bootstrap\Controller\Controller;
+use ActiveCollab\Controller\ActionNameResolver\ActionNameResolverInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+
+class TestController extends Controller
+{
+    public function __construct(ActionNameResolverInterface $action_name_resolver = null, string $action_result_attribute_name = 'action_result', LoggerInterface $logger = null)
+    {
+        if (empty($action_name_resolver)) {
+            $action_name_resolver = new class() implements ActionNameResolverInterface {
+                public function getActionName(ServerRequestInterface $request): string
+                {
+                    return 'index';
+                }
+            };
+        }
+
+        parent::__construct($action_name_resolver, $action_result_attribute_name, $logger);
+    }
+}
