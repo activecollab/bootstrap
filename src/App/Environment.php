26651a39d801962c6db29305203f618c5c824379
Ilija Studen <ilija.studen@activecollab.com>
2019-12-21 17:39:31 +0100

    Add environment value object

---
+++ src/App/Environment.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the Shepherd project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Bootstrap\App;
+
+use ActiveCollab\Bootstrap\App\EnvironmentInterface;
+use InvalidArgumentException;
+use LogicException;
+
+class Environment implements EnvironmentInterface
+{
+    private $environment;
+
+    public function __construct(string $environment)
+    {
+        if (!$this->isValidEnvironment($environment)) {
+            throw new InvalidArgumentException("Value '{$environment}' is not a supported environment.");
+        }
+
+        $this->environment = $environment;
+    }
+
+    public function getEnvironment(): string
+    {
+        return $this->environment;
+    }
+
+    public function setEnvironment(string $environment): EnvironmentInterface
+    {
+        if (!$this->isTest()) {
+            throw new LogicException('Environment can be changed only when in test mode.');
+        }
+
+        if (!$this->isValidEnvironment($environment)) {
+            throw new InvalidArgumentException("Value '{$environment}' is not a supported environment.");
+        }
+
+        $this->environment = $environment;
+
+        return $this;
+    }
+
+    private function isValidEnvironment(string $environment): bool
+    {
+        return in_array($environment, self::VALID_ENVIRONMENTS);
+    }
+
+    public function isProduction(): bool
+    {
+        return $this->environment === self::PRODUCTION;
+    }
+
+    public function isDevelopment(): bool
+    {
+        return $this->environment === self::DEVELOPMENT;
+    }
+
+    public function isTest(): bool
+    {
+        return $this->environment === self::TEST;
+    }
+
+    public function __toString()
+    {
+        return $this->getEnvironment();
+    }
+}
---
+++ src/App/EnvironmentInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Bootstrap project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Bootstrap\App;
+
+interface EnvironmentInterface
+{
+    const PRODUCTION = 'production';
+    const DEVELOPMENT = 'development';
+    const TEST = 'test';
+
+    const VALID_ENVIRONMENTS = [
+        self::PRODUCTION,
+        self::DEVELOPMENT,
+        self::TEST,
+    ];
+
+    public function getEnvironment(): string;
+
+    public function isProduction(): bool;
+    public function isDevelopment(): bool;
+    public function isTest(): bool;
+}
