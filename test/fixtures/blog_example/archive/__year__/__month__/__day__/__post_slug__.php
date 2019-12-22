397f21f4afe04b47a40779978a613b062adb86de
Ilija Studen <ilija.studen@activecollab.com>
2019-12-22 09:23:50 +0100

    Add routing example

---
+++ test/fixtures/blog_example/archive/__year__/__month__/__day__/__post_slug__.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Bootstrap project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+return function () {
+    return '<h1>Post found by slug</h1>';
+};
---
+++ test/fixtures/blog_example/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Bootstrap project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+return '<h1>Awesome blog.</h1>';
