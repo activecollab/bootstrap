d0d68c2ae8b96d9f5d9cb23dd2bdd272b351eb98
Ilija Studen <ilija.studen@activecollab.com>
2019-12-26 15:51:57 +0100

    Make dev:sitemap command simpler

--- src/Command/DevCommand/SitemapCommand.php
+++ src/Command/DevCommand/SitemapCommand.php
@@ -10,37 +10,16 @@ declare(strict_types=1);
 
 namespace ActiveCollab\Bootstrap\Command\DevCommand;
 
-use ActiveCollab\Bootstrap\Router\Retro\Nodes\Directory\DirectoryInterface;
-use ActiveCollab\Bootstrap\Router\Retro\Nodes\NodeInterface;
-use ActiveCollab\Bootstrap\Router\Retro\Pathfinder\PathfinderInterface;
 use ActiveCollab\Bootstrap\Router\Retro\Router;
+use ActiveCollab\Bootstrap\Router\Retro\SitemapLoader\SitemapLoaderInterface;
 use ActiveCollab\Bootstrap\SitemapPathResolver\SitemapPathResolverInterface;
+use Slim\Interfaces\RouteInterface;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SitemapCommand extends DevCommand
 {
-    protected function configure()
-    {
-        parent::configure();
-
-        $this
-            ->addOption(
-                'include-system',
-                '',
-                InputOption::VALUE_NONE,
-                'Include system directories in the structure'
-            )
-            ->addOption(
-                'include-indexes',
-                '',
-                InputOption::VALUE_NONE,
-                'Include index files in the structure, as lines'
-            );
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $root  = (new Router())->scan($this->getSitemapPath());
@@ -49,84 +28,21 @@ class SitemapCommand extends DevCommand
         $table->setHeaders(
             [
                 'Structure',
-                'Type',
                 'Route',
             ]
         );
 
-        $this->recursivelyPopulateRows($root, '', $table, $input);
-
-        $table->render();
-    }
-
-    private function recursivelyPopulateRows(
-        DirectoryInterface $directory,
-        string $indent,
-        Table $table,
-        InputInterface $input
-    ): void
-    {
-        if ($directory->isSystem() && empty($input->getOption('include-system'))) {
-            return;
-        }
-
-        $table->addRow(
-            [
-                $this->getDirectoryStructureContent($directory, $indent),
-                $directory->isSystem() ? '<info>system dir</info>' : 'dir',
-                $this->getNodeRoute($directory),
-            ]
-        );
-
-        foreach ($directory->getSubdirectories() as $subdirectory) {
-            $this->recursivelyPopulateRows($subdirectory, $this->increaseIndent($indent), $table, $input);
-        }
-
-        foreach ($directory->getFiles() as $file) {
-            if ($file->isSystem() && empty($input->getOption('include-system'))) {
-                continue;
-            }
-
-            if ($file->isIndex() && empty($input->getOption('include-indexes'))) {
-                continue;
-            }
-
+        /** @var RouteInterface $route */
+        foreach ($this->getContainer()->get(SitemapLoaderInterface::class)->getLoadedRoutes() as $route) {
             $table->addRow(
                 [
-                    $this->getNodePath($file, $this->increaseIndent($indent)),
-                    $file->isSystem() ? '<info>system file</info>' : 'file',
-                    $this->getNodeRoute($file),
+                    $route->getPattern(),
+                    $route->getPattern(),
                 ]
             );
         }
-    }
 
-    private function getDirectoryStructureContent(DirectoryInterface $directory, string $indent): string
-    {
-        $result = $this->getNodePath($directory, $indent);
-
-        if ($directory->hasIndex()) {
-            $result .= sprintf(' <comment>~ %s</comment>', $directory->getIndex()->getBasename());
-        }
-
-        return $result;
-    }
-
-    private function getNodePath(NodeInterface $node, string $indent): string
-    {
-        return $indent . '/' . $node->getBasename();
-    }
-
-    private function getNodeRoute(NodeInterface $node): string
-    {
-        return (string) $this->getContainer()
-            ->get(PathfinderInterface::class)
-                ->getRoutingPath($node);
-    }
-
-    private function increaseIndent(string $indent): string
-    {
-        return $indent . '    ';
+        $table->render();
     }
 
     protected function getSitemapPath(): string
